198555b263366511ffe31e6e20cd6c428bb2fa73
MaestroX1 <80501974+MaestroX1@users.noreply.github.com>
2024-11-07 00:21:27 +0300

    Update task_2.py

--- Лабораторная работа 3/task_2.py
+++ Лабораторная работа 3/task_2.py
@@ -1,14 +1,10 @@
 # TODO Напишите функцию find_common_participants
 def find_common_participants(s1, s2, razd=','):
     s1, s2 = s1.split(razd), s2.split(razd)
-    s_vsex = s1 + s2
-    s_obsh = []
-    for i in s_vsex:
-        if (i not in s_obsh) and (s_vsex.count(i)>1):
-            s_obsh.append(i)
+    s_obsh = list(set(s1).intersection(s2))
     s_obsh.sort()
     return s_obsh
-
+# Переделал с использованием intersection()
 
 participants_first_group = "Иванов|Петров|Сидоров"
 participants_second_group = "Петров|Сидоров|Смирнов"
